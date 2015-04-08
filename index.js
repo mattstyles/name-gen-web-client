81934483f6eb5aeb4b2514df0e4d4478650d132d
Matt Styles <matt@veryfizzyjelly.com>
2015-04-08 08:48:44 +0100

    #update wait until the db api is ready

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ require( 'babel/register' );
 var app = require( './lib/server' );
 var port = process.env.PORT || process.env.npm_package_config_port || 14320;
 
-app.listen( port );
-console.log( 'Listening on port %s', port );
+app.on( 'ready', function() {
+    app.listen( port );
+    console.log( 'Listening on port %s', port );
+});
